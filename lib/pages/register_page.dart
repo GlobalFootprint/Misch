3f5d148a8b4a8c274d924714811969b74a234c6e
JalenEvans <jalenemmanuelevans@gmail.com>
2024-10-19 11:54:19 -0400

    Something is going wrong with firebase

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_button.dart';
+import '../components/my_text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+  const RegisterPage({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // text controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  // sign up user
+  void signUp() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+
+                // logo
+                Icon(
+                  Icons.message, // TODO: replace with Misch logo
+                  size: 80,
+                  color: Colors.grey[800],
+                ),
+
+                const SizedBox(height: 50),
+
+                // create an account message
+                const Text(
+                  "Let's create an account for you!",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // email textfield
+                MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false
+                ),
+
+                const SizedBox(height: 10),
+
+                // password textfield
+                MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: false
+                ),
+
+                const SizedBox(height: 10),
+
+                // confirm password textfield
+                MyTextField(
+                    controller: confirmPasswordController,
+                    hintText: 'Confirm Password',
+                    obscureText: false
+                ),
+
+                const SizedBox(height: 25),
+
+                // sign in button
+                MyButton(onTap: signUp, text: 'Sign In'),
+
+                const SizedBox(height: 25),
+
+                // register now prompt
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Already a member?'),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Login now',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+}
